c2aabac5bad7ee6fcfc9471306a53e47ad65dde8
Mihael Krpes <mikrpes@vsite.hr>
2024-05-16 16:51:24 +0200

    v07

--- main.cpp
+++ main.cpp
@@ -1,21 +1,68 @@
 #include "main.h"
 #include "rc.h"
 #include <filesystem>
+#include<string>
+#include <iostream>
+
+using namespace Gdiplus;
+
+
+
 
 void main_window::on_paint(HDC hdc) 
 {
+	RECT rc;
+	GetClientRect(*this, &rc);
+	Graphics graphics(hdc);
+	Pen pen(Color(255, 255, 0, 0), 2);
+	Gdiplus::RectF displayRect(rc.left, rc.top, rc.right, rc.bottom);
+	graphics.DrawImage(img, displayRect);
+
+	Gdiplus::StringFormat t;
+	t.SetAlignment(Gdiplus::StringAlignmentCenter);
+	t.SetLineAlignment(Gdiplus::StringAlignmentFar);
+	Gdiplus::Font font(_T("Arial"), 25, Gdiplus::FontStyleBold);
+	Gdiplus::SolidBrush tForm(Gdiplus::Color::Black);
+	graphics.DrawString(fileName.c_str(), -1, &font, displayRect, &t, &tForm);
+
+
+	displayRect.Width = rc.right - 5;
+	displayRect.Height = rc.bottom - 5;
+	tForm.SetColor(Gdiplus::Color::White);
+	graphics.DrawString(fileName.c_str(), -1, &font, displayRect, &t, &tForm);
+	if (img != NULL && isGif) {
+		AdvanceToNextFrame();
+	}
 }
 
 void main_window::on_command(int id) 
 {
 	switch (id) 
 	{
-		case ID_OPEN:
-			break;
+	case ID_OPEN:
+	{
+		TCHAR path[MAX_PATH]; *path = 0;
+		TCHAR filter[] = _T("Image Files (*.jpg;*.jpeg;*.png;*.gif;*.tiff;*.bmp;*.emf)\0*.jpg;*.jpeg;*.png;*.gif;*.tiff;*.bmp;*.emf\0All Files (*.*)\0*.*\0");
+		OPENFILENAME ofn{ sizeof OPENFILENAME };
+		ofn.hwndOwner = *this;
+		ofn.lpstrFile = path;
+		ofn.nMaxFile = MAX_PATH;
+		ofn.lpstrFilter = filter;
+		ofn.Flags = OFN_HIDEREADONLY;
+		if (::GetOpenFileName(&ofn)) {
+			img = Image::FromFile(path);
+			fileName = std::filesystem::path(path).filename();
+			if (isGIF(path)) {
+				frameCount = img->GetFrameCount(&FrameDimensionTime);
+			}
+		}
+		break;
+	}
 		case ID_EXIT:
 			DestroyWindow(*this);
 			break;
 	}
+	InvalidateRect(*this, NULL, true);
 }
 
 void main_window::on_destroy() 
--- main.h
+++ main.h
@@ -3,6 +3,7 @@
 #include "nwpwin.h"
 
 #include <gdiplus.h>
+using namespace Gdiplus;
 
 class gdiplus_application : public vsite::nwp::application
 {
@@ -18,8 +19,36 @@ public:
 };
 
 class main_window : public vsite::nwp::window {
+public:
+	Image* img;
+	std::wstring fileName;
+	UINT frameCount = 1;
+	UINT currentFrame = 0;
+	UINT frameDelay = 0;
+	UINT lastFrameTime = 0;
+	void AdvanceToNextFrame() {
+		if (GetTickCount64() - lastFrameTime > frameDelay) {
+			currentFrame = (currentFrame + 1) % frameCount;
+			img->SelectActiveFrame(&FrameDimensionTime, currentFrame);
+			lastFrameTime = GetTickCount64();
+			InvalidateRect(*this, NULL, false);
+		}
+	}
+	bool isGIF(const TCHAR* filePath) {
+		const TCHAR* dot = _tcsrchr(filePath, _T('.'));
+		if (_tcsicmp(dot, _T(".gif")) == 0) {
+			isGif = true;
+			return true;
+		}
+		else {
+			isGif = false;
+			return false;
+		}
+	}
 protected:
 	void on_paint(HDC hdc) override;
 	void on_command(int id) override;
 	void on_destroy() override;
+private:
+	bool isGif = false;
 };
